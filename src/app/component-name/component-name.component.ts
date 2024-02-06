358a5c8493395a204800cd393ffdf5819e53f6b4
shendy <shendysetiawan234@gmail.com>
2024-02-06 20:10:45 +0700

    [fix]

---
+++ src/app/component-name/component-name.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-name.component.html',
+  styleUrl: './component-name.component.scss'
+})
+export class ComponentNameComponent {
+
+}
